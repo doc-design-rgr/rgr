2378af2326498e8a175cfa2c50b8d4c7b12ae477
IlyaBelik <37375602+IlyaBelik@users.noreply.github.com>
2020-05-31 01:58:40 +0300

    Update AppController.java

--- RGR_doc_design/src/main/java/com/rgr/project/webapp/controller/AppController.java
+++ RGR_doc_design/src/main/java/com/rgr/project/webapp/controller/AppController.java
@@ -5,15 +5,32 @@ import com.rgr.project.webapp.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.rgr.project.entity.CustomerEntity;
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
-public class AppController {
+public class BouquetController {
 
-    @Autowired
-    AppService service;
+    @PostMapping("/boquet/new")
+    public String addNewBouquet(@ModelAttribute("newBouquet") NewBouquetModel newBouquetModel) {
+        BouquetRepository bouquetRepository = findBouquetRepository(newBouquetModel.getRepository());
+        BouquetRepository.addBouquet(newBouquetModel.getBouquet());
+        return "redirect:/login";
+    }
 
-    @GetMapping
-    public String mainScreen(){
-        return "login";
+    private BouquetRepository findBouquetRepository(String repositoryName) {
+        if (repositoryName.equals("From Catalog")) {
+            return new CatalogBouquetRepository();
+        }
+
+        if(repositoryName.equals(("Your custom bouquet")){
+            return new CustomBouquetRepository();
+        }
+
+        throw new IllegalArgumentException(String.format("Find no repository for given repository name [%s]", repositoryName));
     }
 }
