f7c2c8ea767da6a156e35f70d98b6f3ba6d12be1
IlyaBelik <37375602+IlyaBelik@users.noreply.github.com>
2020-05-31 01:59:07 +0300

    Update BouquetRepo.java

--- RGR_doc_design/src/main/java/com/rgr/project/webapp/repository/BouquetRepo.java
+++ RGR_doc_design/src/main/java/com/rgr/project/webapp/repository/BouquetRepo.java
@@ -1,7 +1,33 @@
 package com.rgr.project.webapp.repository;
 
-import com.rgr.project.entity.BouquetEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
 
-public interface BouquetRepo extends JpaRepository<BouquetEntity, Long> {
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Repository
+public class BouquetRepo  {
+
+    private Set<Bouquet> heroes = new HashSet<>();
+
+    @PostConstruct
+    public void init() {
+        bouquets.add(new Bouquet("123", "321", BouquetEntity.bouquet_id));
+        bouquets.add(new Bouquet("rose", "123", FloweEntity.flower_id));
+    }
+
+    public void addBouquet(Bouquet Bouquet) {
+        heroes.add(Bouquet);
+    }
+
+    public Collection<Bouquet> allBouquets() {
+        return new HashSet<>(bouquets);
+    }
+
+
+
 }
