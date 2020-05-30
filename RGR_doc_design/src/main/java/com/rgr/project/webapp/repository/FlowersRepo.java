7e1300d9d967e0b36781a9446177516a32a0824f
IlyaBelik <37375602+IlyaBelik@users.noreply.github.com>
2020-05-31 02:01:05 +0300

    Update FlowersRepo.java

--- RGR_doc_design/src/main/java/com/rgr/project/webapp/repository/FlowersRepo.java
+++ RGR_doc_design/src/main/java/com/rgr/project/webapp/repository/FlowersRepo.java
@@ -2,6 +2,29 @@ package com.rgr.project.webapp.repository;
 
 import com.rgr.project.entity.FlowersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public interface FlowersRepo extends JpaRepository<FlowersEntity, Long> {
+
+    @Repository
+    public class FlowersRepo implements BouquetRepo {
+
+        private List<Flower> flowers = new ArrayList<>();
+
+        @Override
+        public void addBouquet(Flower flower) {
+            Flowers.add(flower);
+        }
+
+        @Override
+        public Collection<Flower> allflowers() {
+            return List.copyOf(flowers);
+        }
+
+        @Override
+        public String getName() {
+            return "Custom Bouquet";
+        }
 }
